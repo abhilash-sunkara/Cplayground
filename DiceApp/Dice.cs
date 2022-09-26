a1f17cf34520c4fdb78c766aed84324dae327baf
abhilash-sunkara <52720913+abhilash-sunkara@users.noreply.github.com>
2022-09-25 23:00:14 -0500

    dice

---
+++ DiceApp/Dice.cs
@@ -0,0 +1,24 @@
+﻿namespace DiceApp;
+
+public class Dice
+{
+    private Random rnd = new Random();
+    private int sides;
+    private int? facingUp = null; 
+    
+    public Dice(int s)
+    {
+        sides = s;
+    }
+
+    private void Roll()
+    {
+        facingUp = rnd.Next(1, sides + 1);
+    }
+
+    public int getUp()
+    {
+        Roll();
+        return (int)facingUp;
+    }
+}
---
+++ DiceApp/DiceRunner.cs
@@ -0,0 +1,14 @@
+﻿namespace DiceApp;
+
+public class DiceRunner
+{
+    private static Dice d = new Dice(6);
+    
+    static void Main()
+    {
+        Console.WriteLine("Your number is " + d.getUp());
+
+
+    }
+}
+
