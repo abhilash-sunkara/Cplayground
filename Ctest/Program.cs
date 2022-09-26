ae1bcca754cadaf596f4253b7773920f395b15cc
abhilash-sunkara <52720913+abhilash-sunkara@users.noreply.github.com>
2022-09-25 22:48:59 -0500

    if

--- Ctest/Program.cs
+++ Ctest/Program.cs
@@ -1,3 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+
+namespace Ctest
+{
+
+    class IfLabs
+    {
+
+        static void Main()
+        {
+            
+            Console.WriteLine("Price :: " + GetDisc(100));
+            Console.WriteLine("Price :: " + GetDisc(3000));
+        }
+
+        public static double GetDisc(double price)
+        {
+            if(price>2000)
+                return price*0.85;
+            return price;
+        }
+
+        
+    }
+    
+    
+}
